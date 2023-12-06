853d9bb051039db7a602894428cfb2a37b906181
Adhitya Laksamana Bayu Adrian <adhityalba@gmail.com>
2023-12-06 20:18:21 +0700

    add JS

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// const inputField = document.getElementById("input-tinggi")
+// const buttonHitung = document.getElementById("button-hitung")
+
+// inputField.addEventListener('input', function(){
+//     var textInput = inputField.value.trim()
+//     if (validateInput(textInput)) {
+//         buttonHitung.disabled = false
+//     } else {
+//         buttonHitung.disabled = true
+//     }
+// })
+
+// function validateInput(inputValue) {
+//     const integerRegex = /^-?\d+$/;
+//     return integerRegex.test(inputValue)
+// }
+
+const myArray = [6,2,9]
+
+console.log(myArray[3])
