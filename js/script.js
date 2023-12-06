9148639c72608fc8b5e7a9b0ae297fc3df114138
Adhitya Laksamana Bayu Adrian <adhityalba@gmail.com>
2023-12-06 20:20:14 +0700

    uncomment code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
-// const inputField = document.getElementById("input-tinggi")
-// const buttonHitung = document.getElementById("button-hitung")
+const inputField = document.getElementById("input-tinggi")
+const buttonHitung = document.getElementById("button-hitung")
 
-// inputField.addEventListener('input', function(){
-//     var textInput = inputField.value.trim()
-//     if (validateInput(textInput)) {
-//         buttonHitung.disabled = false
-//     } else {
-//         buttonHitung.disabled = true
-//     }
-// })
+inputField.addEventListener('input', function(){
+    var textInput = inputField.value.trim()
+    if (validateInput(textInput)) {
+        buttonHitung.disabled = false
+    } else {
+        buttonHitung.disabled = true
+    }
+})
 
-// function validateInput(inputValue) {
-//     const integerRegex = /^-?\d+$/;
-//     return integerRegex.test(inputValue)
-// }
-
-const myArray = [6,2,9]
-
-console.log(myArray[3])
+function validateInput(inputValue) {
+    const integerRegex = /^-?\d+$/;
+    return integerRegex.test(inputValue)
+}
